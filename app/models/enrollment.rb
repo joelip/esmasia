131181dc517110eebb2e6750e6a201be0b13301e
Joe Lipper <lipperjj@gmail.com>
2014-07-11 18:52:28 -0700

    updated design and added course date and type to form submission

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,5 @@
 class Enrollment < ActiveRecord::Base
   attr_accessible :city, :dob, :grade, :home_address, :parent_email, :parent_full_name,
   :state, :student_email, :student_full_name, :student_high_school, :student_mobile_number
+  :course_date, :course_type
 end
